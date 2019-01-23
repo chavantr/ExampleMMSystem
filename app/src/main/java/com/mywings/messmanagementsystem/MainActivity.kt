48630aebcdef0a7fe58e84eaf666e3a83d264fcd
Tatyabhau R. Chavan <tatyabhau.chavan@gmail.com>
2019-01-23 21:17:25 +0530

    intent

--- app/src/main/java/com/mywings/messmanagementsystem/MainActivity.kt
+++ app/src/main/java/com/mywings/messmanagementsystem/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.mywings.messmanagementsystem
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnSignUp.setOnClickListener {
+            val intent = Intent(this@MainActivity, RegistrationActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
