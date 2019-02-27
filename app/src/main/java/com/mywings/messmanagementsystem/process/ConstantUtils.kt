907a146df4f373dd5d70534d9403c7a779d2279e
Tatyabhau R. Chavan <tatyabhau.chavan@gmail.com>
2019-02-27 07:25:06 +0530

    w e b s e r v i c e s

--- app/src/main/java/com/mywings/messmanagementsystem/process/ConstantUtils.kt
+++ app/src/main/java/com/mywings/messmanagementsystem/process/ConstantUtils.kt
@@ -1,9 +1,10 @@
 package com.mywings.messmanagementsystem.process
 
 object ConstantUtils {
-    const val URL = "http://192.168.43.157:8033/MyRestService.svc/"
-    const val LOGIN = "Login"
-    const val REGISTRATION = "Registration"
-    const val GET_MESS = "GetMess"
+    //const val URL = "http://192.168.43.157:8033/MyRestService.svc/"
+    const val URL = "http://mobile.punetraining.com/MyRestService.svc/"
+    const val LOGIN = "MMS_Login"
+    const val REGISTRATION = "MMS_Registration"
+    const val GET_MESS = "MMS_GetMess"
     const val ESTA_CON = "EstablishConnection"
 }
